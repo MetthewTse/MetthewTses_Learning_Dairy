e88cf3d50e65f87a21226706e40014f76526f585
xiefei <hh56105@foxmail.com>
2023-12-26 17:08:29 +0800

    算法序号-001，三数之和，难度中等

---
+++ src/main/java/algorithm/Algorithm_15_threeSum.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 15. 三数之和
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+ * 你返回所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ * 示例 1：
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 解释：
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ * 注意，输出的顺序和三元组的顺序并不重要。
+ *
+ *
+ */
+public class Algorithm_15_threeSum {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> solution(int[] nums){
+        Set<Integer> numsSet = new HashSet<>();
+        if (nums.length==0){
+            return new ArrayList<>();
+        }
+
+        for (int i=0;i<nums.length;i++) {
+
+        }
+        return new ArrayList<>();
+    }
+}
